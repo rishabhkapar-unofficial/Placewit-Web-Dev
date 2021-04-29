fe639dc74d32fa64f0f33988215a23d2e49b0f5c
rishabhkapar <rishabhkapar.unofficial@gmail.com>
2021-04-29 22:39:21 +0530

    Removed unnecessary cors values

--- Class 14 - Mongo DB Backend/backend/index.js
+++ Class 14 - Mongo DB Backend/backend/index.js
@@ -8,7 +8,6 @@ const app = express();
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin','*');
     res.setHeader('Access-Control-Allow-Methods','GET, POST, PATCH, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     next();
 }); 
 
